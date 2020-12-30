4fd6dfbea2be2c516e92c71243ad5207acc0be39
Miguel Rivas Perez <mrivasperez1996@gmail.com>
2020-12-29 21:29:50 -0800

    first commit

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,11 @@
+const Navbar = (props) => {
+  return (
+    <nav className="navbar bg-primary">
+      <h1>
+        <i className={props.icon} /> {props.title}
+      </h1>
+    </nav>
+  );
+};
+
+export default Navbar;
